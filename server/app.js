d4e3e2667df0f5e95ed3eab71e276b0e1e5f8a6a
Pragistyo <new.SOS.hope@outlook.com>
2017-11-02 19:42:32 +0700

    add mongodb atlas

--- server/app.js
+++ server/app.js
@@ -6,6 +6,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+mongoose.connect('mongodb://admin:123@cluster0-shard-00-00-jgxqq.mongodb.net:27017,cluster0-shard-00-01-jgxqq.mongodb.net:27017,cluster0-shard-00-02-jgxqq.mongodb.net:27017/bedekan?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err) => {
+    if (!err) {
+        console.log('CONNECTED mongoose');
+    } else {
+        console.log('NOT CONNECT mongoose ', err);
+    }
+})
+
 const app = express();
 
 app.use(cors());
